3432ed47911571c7f4c3b707d842fd16800700a9
mrech <rivato.morena@gmail.com>
2019-08-30 10:20:29 +0200

    Features Standardization

--- HousePrice.py
+++ HousePrice.py
@@ -5,6 +5,7 @@ import numpy as np
 import seaborn as sns
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, RobustScaler, MaxAbsScaler
 
 # 0. Import dataset
 #
@@ -51,20 +52,19 @@ def summary_stats_category(X):
     plt.show()
 
 
-def summary_stats_numeric(X):
+def summary_stats_numeric(X, y):
     '''
-    Plot summary stats together with univariate distribution.
+    Plot summary stats together with univariate dist and bivariate dist
     Input: numerical variable
     Output: plot and summary stats
     '''
 
     # Remove missing
+    y = y[np.where(~pd.isna(X))[0]]
     X = X.dropna()
-    # X['LotFrontage']
-    #
 
     # Create a figure instance, and the two subplots
-    fig = plt.figure()
+    fig = plt.figure(figsize=(50, 100))
     fig.suptitle('Univariate distribution & descriptive statistics\n{}'.format(
         round(pd.DataFrame.describe(X), 2)))
     ax1 = fig.add_subplot(121)
@@ -72,9 +72,9 @@ def summary_stats_numeric(X):
     fig.subplots_adjust(top=0.75)
 
     # Plot on ax1 with the ax argument
-    sns.boxplot(X, ax=ax1)
-    sns.distplot(X, ax=ax2)
-
+    sns.distplot(X, ax=ax1)
+    sns.regplot(X, y, ax=ax2)
+    
     plt.show()
 
 
@@ -244,19 +244,21 @@ col_miss = col_miss[col_miss['N_missing'] > 0]
 
 # summary_stats_category(X['MasVnrType'])
 # bivariate_distr_categorical(X['Electrical'])
-# summary_stats_numeric(X['MasVnrArea'])
+# summary_stats_numeric(X['MasVnrArea'], y)
 # summary_stats_category(X['Electrical'])
 
 cat_var = X.select_dtypes(include=['category']).columns
 num_var = X.select_dtypes(include=['int', 'float']).columns
 
 # scatterplot of two variable, regression line and 95% confidence
-# Adjust for OUTLIERS
+# Adjust for OUTLIERS and HIGH LEVERAGE POINTS 
+# (laverage stats with multiple predictors)
 # for i in num_var:
 #    sns.regplot(X[i], y)
 #    plt.show()
 
 # compute correlation matrix
+# 
 corr = X.corr()
 
 # Generate a mask for the upper triangle
@@ -270,7 +272,8 @@ fig, ax = plt.subplots(figsize=(50, 100))
 cmap = sns.diverging_palette(220, 10, as_cmap=True)
 
 # Draw the heatmap with the mask
-# Adjust for COLLINEARITY
+# Adjust for COLLINEARITY/Multicollinearity
+# compute the variance inflation factor
 sns.heatmap(corr, mask=mask, cmap=cmap, center=0, linewidths=.5)
 plt.xticks(rotation=90)
 plt.yticks(rotation=45)
@@ -284,12 +287,11 @@ imp_cat_miss = SimpleImputer(strategy='most_frequent')
 imp_cat_miss = imp_cat_miss.fit(X[cat_var])
 X[cat_var] = imp_cat_miss.transform(X[cat_var])
 
-summary_stats_numeric(X['LotFrontage'])
+summary_stats_numeric(X['LotFrontage'], y)
 
 # Generalize for all numerical
 # assign median value to missing in LotFrontage
 
-num_var = X.select_dtypes(include=['int', 'float']).columns
 imp_num_miss = SimpleImputer(missing_values=np.nan, strategy='median')
 imp_num_miss = imp_num_miss.fit(X[num_var])
 
@@ -348,17 +350,87 @@ X = X.drop(cat_list, axis = 1)
 
 X = pd.DataFrame.join(X, pd.DataFrame(cat))
 
-# 2.4 Feature Normalization
-# sin cos ciclic feature Month (MoSold) date_list[-1]
-# 1st strategy
-# date_list[:-1] normalize
+# 2.4 Features Standardization
+# https://scikit-learn.org/stable/modules/preprocessing.html#preprocessing-scaler
+# 1st strategy, take into account for sparsity and ourliers
+# Extract variables with sparse data where at least 25% of observations is 0
+descriptive = pd.DataFrame.describe(X[num_var]) 
+sparse = descriptive.loc['25%', :] == 0
+sparse_var = sparse[np.where(sparse)[0]].index.tolist()
+
+sparse_transf = MaxAbsScaler().fit(X[sparse_var])
+X[sparse_var] = sparse_transf.transform(X[sparse_var])
+
+#for i in sparse_var:
+#    summary_stats_numeric(X[i], y)
+
+num_var = num_var.tolist()
+
+# list comprehension
+num_var = [i for i in num_var if i not in sparse_var]
+descriptive = pd.DataFrame.describe(X[num_var]) 
+
+# Standardize variable with very small standard deviations
+small_std = descriptive.loc['std', :] < 5
+small_std_var = small_std[np.where(small_std)[0]].index.tolist()
+
+small_std_transf = MaxAbsScaler().fit(X[small_std_var])
+X[small_std_var] = small_std_transf.transform(X[small_std_var])
+
+# Standardize variables with outliers (Skewed distribution)
+num_var = [i for i in num_var if i not in small_std_var]
+
+skewed_transf = RobustScaler().fit(X[num_var])
+X[num_var] = skewed_transf.transform(X[num_var])
+
+# Standardize date
+descriptive = pd.DataFrame.describe(X)
+descriptive = descriptive.loc[['mean', 'std'],:].transpose()
+descriptive.sort_values(by=['mean', 'std'])
+
+#for i in date_list:
+#    summary_stats_numeric(X[i],y)
+# 0 imput to GarageYrBlt (feature engineer to create meaningfull variable and handle multicoll.)
+
+yr_list = date_list[:-1]
+
+yr_stand_transf = StandardScaler().fit(X[yr_list])
+X[yr_list] = yr_stand_transf.transform(X[yr_list])
+
+# Maintaining the Cyclic Representations of Month Sold
 # https://datascience.stackexchange.com/a/24003
 
+## Explanation / Intuition
+#foo = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+## Circle Circumference = (2*pi*r) devide by 12 periods
+## for each period we calculate sin and cos
+## With this transformation any cyclical feature will be doubled. 
+#bar1 = np.cos((foo-1) * (2*np.pi/12))
+#bar2 = np.sin((foo-1) * (2*np.pi/12))
+#fig, ax = plt.subplots()
+#plt.scatter(bar1, bar2)
+#for i, txt in enumerate(foo-1):
+#    ax.annotate(txt, (bar1[i], bar2[i]))
+#plt.show()
+
+MoSold_cos = np.cos((X[date_list[-1]]-1) * (2*np.pi/12))
+MoSold_sin = np.sin((X[date_list[-1]]-1) * (2*np.pi/12))
+
+fig, ax = plt.subplots()
+plt.scatter(MoSold_cos, MoSold_sin)
+for i, txt in enumerate((X[date_list[-1]]-1)):
+    ax.annotate(txt, (MoSold_cos[i], MoSold_sin[i]))
+plt.show()
+
+X = X.drop(date_list[-1], axis = 1)
+X['MoSold_cos'] = MoSold_cos
+X['MoSold_sin'] = MoSold_sin
 
 # TO DO
 # 2.3.1 Log transformation
 # https://stats.stackexchange.com/questions/18844/when-and-why-should-you-take-the-log-of-a-distribution-of-numbers
 # FEATURE ENGINEERING
+# Group by category - create summary variables (ex. garage) to solve for MULTICOLLINEARITY
 # boxplot by categories
 #X['MoYrSold'] = X['MoSold'].map(str) + '-' + X['YrSold'].map(str)
 #X['MoYrSold'] = pd.to_datetime(X['MoYrSold'], format='%m-%Y')
